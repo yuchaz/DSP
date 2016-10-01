dc4b3e036091eccfe2e56c0e152a625f2fcd138a
Yu-Chia Chen <yuchaz@uw.edu>
2016-10-01 16:58:37 -0700

    Finish M2 but need to make it clearer, contain more info on the axises...etc

---
+++ hw1/M2_impulse_and_freq_response.m
@@ -0,0 +1,15 @@
+SIZE_OF_SAMPLE = 51
+
+a = [1 -1.7163 1.1724 -0.2089]
+b = [0.5264 -1.5224 1.5224 -0.5264]
+
+impulse = [1 zeros(1,SIZE_OF_SAMPLE-1)]
+
+impulse_response = filter(b,a,impulse)
+impulse_response_axis = 0:SIZE_OF_SAMPLE-1
+subplot(2,1,1)
+plot(impulse_response_axis, impulse_response)
+
+subplot(2,1,2)
+[freq_response, w] = freqz(b,a)
+plot(w/pi,20*log10(abs(freq_response)))
