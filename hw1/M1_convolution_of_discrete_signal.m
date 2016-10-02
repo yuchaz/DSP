93b45c519e2d76a28ef11a1d3a7908c16185f761
Yu-Chia Chen <yuchaz@uw.edu>
2016-10-01 17:25:31 -0700

    Clarify the figs of both M1 and M2

--- hw1/M1_convolution_of_discrete_signal.m
+++ hw1/M1_convolution_of_discrete_signal.m
@@ -1,4 +1,3 @@
-figure
 h = [1 1 1 -1 1]
 x = [1 -1 1 1 1]
 h_start_point = 0
@@ -9,4 +8,8 @@ y_start_point = min([x_start_point,h_start_point])
 y_end_point = y_start_point + size(y,2) - 1
 y_axis = y_start_point:y_end_point
 
-stem(y_axis, y)
+stem(y_axis, y, 'filled')
+xlabel('n')
+ylabel('Magnitude')
+title('Convolution of Two signals')
+xlim([-0.5,8.5])
