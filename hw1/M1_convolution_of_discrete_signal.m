7bad7fb0c483e8b32d951750e44005e280432dba
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-06 11:34:40 -0700

    Add semicon to the end of each line

--- hw1/M1_convolution_of_discrete_signal.m
+++ hw1/M1_convolution_of_discrete_signal.m
@@ -1,15 +1,15 @@
-h = [1 1 1 -1 1]
-x = [1 -1 1 1 1]
-h_start_point = 0
-x_start_point = 0
+h = [1 1 1 -1 1];
+x = [1 -1 1 1 1];
+h_start_point = 0;
+x_start_point = 0;
 
-y = conv(h,x)
-y_start_point = min([x_start_point,h_start_point])
-y_end_point = y_start_point + size(y,2) - 1
-y_axis = y_start_point:y_end_point
+y = conv(h,x);
+y_start_point = min([x_start_point,h_start_point]);
+y_end_point = y_start_point + size(y,2) - 1;
+y_axis = y_start_point:y_end_point;
 
-stem(y_axis, y, 'filled')
-xlabel('n')
-ylabel('Magnitude')
-title('Convolution of Two signals')
-xlim([-0.5,8.5])
+stem(y_axis, y, 'filled');
+xlabel('n');
+ylabel('Magnitude');
+title('Convolution of Two signals');
+xlim([-0.5,8.5]);
--- hw1/M2_impulse_and_freq_response.m
+++ hw1/M2_impulse_and_freq_response.m
@@ -1,22 +1,22 @@
-SIZE_OF_SAMPLE = 51
+SIZE_OF_SAMPLE = 51;
 
-a = [1 -1.7163 1.1724 -0.2089]
-b = [0.5264 -1.5224 1.5224 -0.5264]
+a = [1 -1.7163 1.1724 -0.2089];
+b = [0.5264 -1.5224 1.5224 -0.5264];
 
-impulse = [1 zeros(1,SIZE_OF_SAMPLE-1)]
+impulse = [1 zeros(1,SIZE_OF_SAMPLE-1)];
 
-impulse_response = filter(b,a,impulse)
-impulse_response_axis = 0:SIZE_OF_SAMPLE-1
-subplot(2,1,1)
-stem(impulse_response_axis, impulse_response)
-title('Impulse response')
-xlabel('n')
-ylabel('Magnitude')
+impulse_response = filter(b,a,impulse);
+impulse_response_axis = 0:SIZE_OF_SAMPLE-1;
+subplot(2,1,1);
+stem(impulse_response_axis, impulse_response);
+title('Impulse response');
+xlabel('n');
+ylabel('Magnitude');
 
-subplot(2,1,2)
-[freq_response, w] = freqz(b,a)
-plot(w/pi,20*log10(abs(freq_response)))
-title('Frequency response')
-xlabel('Normalized Frequency (\times\pi rad/sample)')
-ylabel('Magnitude (dB)')
-ylim([-70,10])
+subplot(2,1,2);
+[freq_response, w] = freqz(b,a);
+plot(w/pi,20*log10(abs(freq_response)));
+title('Frequency response');
+xlabel('Normalized Frequency (\times\pi rad/sample)');
+ylabel('Magnitude (dB)');
+ylim([-70,10]);
