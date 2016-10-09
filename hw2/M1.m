7e86653702858a45096a98129b7e77d7eafc9b31
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-08 18:53:30 -0700

    Finish the matrix calculating part

--- hw2/M1.m
+++ hw2/M1.m
@@ -1,10 +1,20 @@
-b = [4 0 0 0 0];
-denominator_1 = [2 1]
-denominator_2 = [1 -2]
-denominator_3 = [1 -3]
-a = conv(conv(conv(deno_1, deno_1), deno_2), deno_3);
+b = [1 0 0 0 0];
+a_1 = [1 1/2];
+a_2 = [1 -2];
+a_3 = [1 -3];
+a = conv(conv(conv(a_1, a_1), a_2), a_3);
 [r_a, p_a, k_a] = residuez(b, a);
 
+A = conv(conv(a_1,a_1),a_2);
+B = conv(conv(a_1,a_1),a_3);
+C = conv(conv(a_1,a_2),a_3);
+D = conv(a_2,a_3);
+
+M = [A;B;C;[D 0]];
+M_transpose = transpose(M)
+
+Mi = inv(M_transpose)
+
+output = Mi * [1;0;0;0]
 r_a
-p_a
-k_a
+
