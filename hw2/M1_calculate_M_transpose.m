c77f52bf5f9bdbb824ce847a89ce801a8effd92e
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-12 14:51:18 -0700

    seperate M1 into two, the hand done part and residuez part

--- hw2/M1_calculate_M_transpose.m
+++ hw2/M1_calculate_M_transpose.m
@@ -1,9 +1,6 @@
-b = [1 0 0 0 0];
 a_1 = [1 1/2];
 a_2 = [1 -2];
 a_3 = [1 -3];
-a = conv(conv(conv(a_1, a_1), a_2), a_3);
-[r_a, p_a, k_a] = residuez(b, a);
 
 A = conv(conv(a_1,a_1),a_2);
 B = conv(conv(a_1,a_1),a_3);
@@ -16,5 +13,3 @@ M_transpose = transpose(M)
 Mi = inv(M_transpose)
 
 output = Mi * [1;0;0;0]
-r_a
-
