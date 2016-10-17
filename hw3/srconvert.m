0d547e01953f54f4cecaf4c1513320905aca3a19
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-16 21:19:01 -0700

    Test ifir filter but fail...

--- hw3/srconvert.m
+++ hw3/srconvert.m
@@ -4,8 +4,13 @@ DOWN_FACTOR = 147;
 
 uped_sig = upsample(in, UP_FACTOR);
 E = poly1(uped_sig,DOWN_FACTOR);
-a = 1;
-b = [1/4 1/4 1/4 1/4];
+%a = 1;
+%b = [1/4 1/4 1/4 1/4];
+
+[h,g]=ifir(DOWN_FACTOR,'low',[.0027 .0031],[.5 .001]);
+%lp = fftfilt(h,g);
+lp=g;
+LPF = poly1(lp, DOWN_FACTOR);
 
 [h,g]=ifir(147,'low',[.0035 .0037],[.01 .0001]);
 %lp = fftfilt(h,g)
@@ -14,20 +19,18 @@ lp=h;
 filtered_matrix = {};
 filtered_matrix{size(E,1),1} = [];
 for i=1:size(E,1)
-    filtered_sig = filter(b,a,E(i,:));
+    %filtered_sig = filter(b,a,E(i,:));
+    filtered_sig = fftfilt(LPF(i,:),E(i,:));
     filtered_matrix{i,1} = downsample(filtered_sig, DOWN_FACTOR);
 end;
 
 FM = cell2mat(filtered_matrix);
 
 out = transpose(FM(:));
-%uped_signal = upsample(in, UP_FACTOR);
+
+
 
 %[h,g]=ifir(UP_FACTOR,'low',[.003 .0031],[.01 .001]);
 %lp = fftfilt(h,g)
-
 %filtered_signal = fftfilt(lp,uped_signal);
-
-%out = downsample(filtered_signal, DOWN_FACTOR);
-
 return;
