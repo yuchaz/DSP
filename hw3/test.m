ff9c326f9f1e230d2482b4211e9cf58394dc1634
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-17 13:09:36 -0700

    Test high M=147

--- hw3/test.m
+++ hw3/test.m
@@ -1,7 +1,9 @@
-UP_RATE = 12;
-DOWN_RATE = 7;
-[h,g]=ifir(DOWN_RATE,'low',[.12 .14],[.01 .001]);
-input = [1 zeros(1,300)];
+UP_RATE = 147;
+DOWN_RATE = 147;
+%[h,g]=ifir(DOWN_RATE,'low',[.12 .14],[.01 .001]);
+[h,g]=ifir(DOWN_RATE,'low',[.0035 .0037],[.01 .0001]);
+
+input = [1 zeros(1,3000)];
 
 E = poly1(upsample(input,UP_RATE),DOWN_RATE);
 
@@ -19,7 +21,7 @@ output = transpose(LOW_SIG_M(:));
 desired_output = fftfilt(h,input);
 O = dfilt.dffir(output);
 DO = dfilt.dffir(desired_output);
-hfv = fvtool(DO);
+hfv = fvtool(DO,O);
 legend(hfv,'Desired Output', 'Implemented Output');
 
 
