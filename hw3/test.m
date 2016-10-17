57606d9f1ada45eec610d49b28c9e9ae75502bff
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-17 12:33:46 -0700

    Why I make it I don't know...

--- hw3/test.m
+++ hw3/test.m
@@ -1,15 +1,17 @@
 [h,g]=ifir(4,'low',[.12 .14],[.01 .001]);
 input = [1 zeros(1,300)];
-E = poly1(input,4);
+
+E = poly1(upsample(input,4),4);
+%E = poly1(input,4);
 
 LPF = poly1(h, 4);
 LOW_SIG{size(E,1),1} = [];
 
 for i=1:size(E,1)    
+    LOW_SIG{i,1} = fftfilt(downsample(E(i,:),4),LPF(i,:));
     
-    poly_with_down = downsample(filtered_sig,4);
-    
-    LOW_SIG{i,1} = upsample(poly_with_down,4);
+    %filtered_sig = fftfilt(E(i,:),LPF(i,:));
+    %LOW_SIG{i,1} = upsample(downsample(filtered_sig,4),4);
 end;
 
 LOW_SIG_M = cell2mat(LOW_SIG);
@@ -19,8 +21,8 @@ output = transpose(LOW_SIG_M(:));
 desired_output = fftfilt(h,input);
 O = dfilt.dffir(output);
 DO = dfilt.dffir(desired_output);
-hfv = fvtool(O, DO);
-legend(hfv,'Implmented Output', 'Desired output');
+hfv = fvtool(O);
+legend(hfv,'Desired Output', 'Implemented Output');
 
 
 
