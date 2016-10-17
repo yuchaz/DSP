8e68adf5d3726ea6bac31ba3fcf95588941b8e2e
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-16 20:28:32 -0700

    Try to use ifir to solve it but run out of memory!

--- hw3/srconvert.m
+++ hw3/srconvert.m
@@ -1,9 +1,15 @@
-function out_signal=srconvert(in_signal, al)
+function out_signal=srconvert(in_signal)
 
 upsampled_signal = upsample(in_signal, 320);
+
+[h,g]=ifir(320,'low',[.003 .0031],[.01 .001]);
+lp = fftfilt(h,g)
+
 %h = firls(555, [0,1/400,1/320,1],[1,1,0,0]);
-h = kaiser(3000, al)
-filtered_signal = fftfilt(h,upsampled_signal);
+%h = intfilt(300,10,0.5);
+%h = kaiser(1001, al)
+%filtered_signal = filter(h,1,upsampled_signal);
+filtered_signal = fftfilt(lp,upsampled_signal);
 
 out_signal = downsample(filtered_signal, 147);
 
