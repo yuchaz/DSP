76db10dd13b43980f10dfd6239853fc471075b0f
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-17 12:56:29 -0700

    It still works it it's upsampling... still don't know why.

--- hw3/test.m
+++ hw3/test.m
@@ -1,17 +1,15 @@
-[h,g]=ifir(4,'low',[.12 .14],[.01 .001]);
+UP_RATE = 12;
+DOWN_RATE = 7;
+[h,g]=ifir(DOWN_RATE,'low',[.12 .14],[.01 .001]);
 input = [1 zeros(1,300)];
 
-E = poly1(upsample(input,4),4);
-%E = poly1(input,4);
+E = poly1(upsample(input,UP_RATE),DOWN_RATE);
 
-LPF = poly1(h, 4);
+LPF = poly1(h, DOWN_RATE);
 LOW_SIG{size(E,1),1} = [];
 
 for i=1:size(E,1)    
-    LOW_SIG{i,1} = fftfilt(downsample(E(i,:),4),LPF(i,:));
-    
-    %filtered_sig = fftfilt(E(i,:),LPF(i,:));
-    %LOW_SIG{i,1} = upsample(downsample(filtered_sig,4),4);
+    LOW_SIG{i,1} = fftfilt(LPF(i,:),downsample(E(i,:),DOWN_RATE));
 end;
 
 LOW_SIG_M = cell2mat(LOW_SIG);
@@ -21,7 +19,7 @@ output = transpose(LOW_SIG_M(:));
 desired_output = fftfilt(h,input);
 O = dfilt.dffir(output);
 DO = dfilt.dffir(desired_output);
-hfv = fvtool(O);
+hfv = fvtool(DO);
 legend(hfv,'Desired Output', 'Implemented Output');
 
 
