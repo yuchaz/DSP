196637f8621a62d879f6bad912aa8d1c29e20fe9
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-16 22:53:45 -0700

    Fix the small bug, which have to downsample first than pass to LPF

--- hw3/srconvert.m
+++ hw3/srconvert.m
@@ -5,18 +5,19 @@ DOWN_FACTOR = 147;
 uped_sig = upsample(in, UP_FACTOR);
 E = poly1(uped_sig,DOWN_FACTOR);
 
-[h,g]=ifir(DOWN_FACTOR,'low',[.0035 .0037],[.01 .0001]);
-lp=h;
+[lp,g]=ifir(DOWN_FACTOR,'low',[.0035 .0037],[.01 .0001]);
 LPF = poly1(lp, DOWN_FACTOR);
 
 filtered_matrix = {};
 filtered_matrix{size(E,1),1} = [];
+
 for i=1:size(E,1)
-    filtered_sig = fftfilt(LPF(i,:),E(i,:));
-    filtered_matrix{i,1} = downsample(filtered_sig, DOWN_FACTOR);
+    down_sig = downsample(E(i,:), DOWN_FACTOR);
+    filtered_matrix{i,1} = fftfilt(LPF(i,:),down_sig);
 end;
 
 FM = cell2mat(filtered_matrix);
+
 out = transpose(FM(:));
 
 return;
