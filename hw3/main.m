102785809f879c8d4cabef75a13a527b7fad1b00
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-19 21:19:23 -0700

    start to check the avaiablilty myself

--- hw3/main.m
+++ hw3/main.m
@@ -1,2 +1,12 @@
 output = srconvert([1 zeros(1,3000)]);
-verify(output)
+%verify(output)
+X_db = mag2db(abs(fft(output)));
+plot(linspace(0,2,size(X_db,2)), X_db);
+
+for i=1:size(X_db,2)
+    if X_db(1,i) < -3 && i/size(X_db,2) > 0.2
+        break
+    end
+end
+
+cutoff_freq = 2*i/size(X_db,2);
