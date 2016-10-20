e532ad565a7177084e21fb493c1ce6ed48b7bebc
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-20 01:32:50 -0700

    Make old lpf_create_to_compare

--- hw3/lpf_create.m
+++ hw3/lpf_create.m
@@ -1,8 +1,8 @@
 function lpf = lpf_create(down_factor)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-[h,g]=ifir(down_factor,'low',[.002893 .0033],[.1 .1]);
-lpf = conv(h,g);
+[h,g]=ifir(down_factor,'low',[.0033 .0037],[.01 .0001]);
+lpf = h;
 
 end
 
