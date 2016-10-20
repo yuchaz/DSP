cb04da4f1f5fb1385096f902aef8e1b0e40a7dbf
yuchaz <erwin.yuchia.chen@gmail.com>
2016-10-20 00:56:41 -0700

    Convert sig successfully! but have some small noises

--- hw3/resample_wagner.m
+++ hw3/resample_wagner.m
@@ -1,8 +1,7 @@
 [x,~] = audioread('Wagner.wav');
 y = srconvert(x.');
 audiowrite('converted.wav',y, 24000);
-subplot(2,1,1);
-plot(x);
-subplot(2,1,2);
-plot(y);
-sound(y,24000);
+%subplot(2,1,1);
+%plot(x);
+%subplot(2,1,2);
+%plot(y);
