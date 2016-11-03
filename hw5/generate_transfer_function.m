8c545a5d17cbf0d2aa764b07a598ec6ab144bfae
YuChia <erwin.yuchia.chen@gmail.com>
2016-11-02 18:33:56 -0700

    Delete the comment on function

--- hw5/generate_transfer_function.m
+++ hw5/generate_transfer_function.m
@@ -1,6 +1,5 @@
 function [ b,a ] = generate_transfer_function()
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+
 b_1 = [1 1];
 b_2 = [1 -1.0166 1];
 b = 0.05634*conv(b_1,b_2);
@@ -10,4 +9,3 @@ a_2 = [1 -1.4461 0.7957];
 a = conv(a_1,a_2);
 
 end
-
