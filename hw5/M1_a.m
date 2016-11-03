9a89fce10ee74aa380eead272696144770150cb3
yuchaz <erwin.yuchia.chen@gmail.com>
2016-11-02 18:05:01 -0700

    Format the output files

--- hw5/M1_a.m
+++ hw5/M1_a.m
@@ -3,3 +3,6 @@
 zeros = roots(b);
 poles = roots(a);
 
+outfile = fopen('output/zeros_poles.dat', 'w');
+fprintf(outfile, 'Zeros are: %s\n', num2str(zeros.'));
+fprintf(outfile, 'Poles are: %s\n', num2str(poles.'));
--- hw5/M1_d.m
+++ hw5/M1_d.m
@@ -1,3 +1,8 @@
 [b,a] = generate_transfer_function();
 
 [r,p,k] = residuez(b,a);
+
+outfile = fopen('output/r_p_k.dat', 'w');
+fprintf(outfile, 'r =  [%s]\n', num2str(r.'));
+fprintf(outfile, 'p =  [%s]\n', num2str(p.'));
+fprintf(outfile, 'k =  [%s]\n', num2str(k.'));
