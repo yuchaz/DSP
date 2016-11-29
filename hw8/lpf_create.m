2f70fbb22bac4d74ec19c6abf3971ac5f0f66b39
yuchaz <erwin.yuchia.chen@gmail.com>
2016-11-29 13:05:14 -0800

    Finsih lpf using pm method, but need to do more...

--- hw8/lpf_create.m
+++ hw8/lpf_create.m
@@ -1,8 +1,11 @@
 function lpf = lpf_create(freq)
-disp(freq);
-[h,g]=ifir(freq,'low',[0.86/(freq+1) 1/(freq+1)],[.1 .1]);
-lpf = conv(h,g);
+%disp(freq);
+%[h,g]=ifir(freq,'low',[0.86/(freq+1) 1/(freq+1)],[.1 .1]);
+%lpf = conv(h,g);
 
+f = [0 0.86/(freq) 1/(freq) 1];
+a = [1 1 0 0];
+lpf = firpm(40,f,a);
 
 end
 
